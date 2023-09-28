1d326618876ca8a575e93fae0a8590c937e9e79d
xbucks <jmy.kot@gmail.com>
2023-09-28 13:48:55 +0900

    feat: add systray icon

--- src/main.rs
+++ src/main.rs
@@ -46,10 +46,61 @@ fn handle_client(mut stream: TcpStream) {
     }
 }
 
+fn build_tray() -> Result<(), systray::Error> {
+    let mut app;
+    match systray::Application::new() {
+        Ok(w) => app = w,
+        Err(_) => panic!("Can't create window!"),
+    }
+
+    match app.set_icon_from_file("./resources/appicon_512x512.png") {
+        Ok(..) => {},
+        Err(err) => {
+            println!("{}", err);
+        }
+    };
+    app.set_tooltip(&"Whatever".to_string())?;
+
+    app.add_menu_item("Print a thing", |_| {
+        println!("Printing a thing!");
+        Ok::<_, systray::Error>(())
+    })?;
+
+    app.add_menu_item("Add Menu Item", |window| {
+        window.add_menu_item("Interior item", |_| {
+            println!("what");
+            Ok::<_, systray::Error>(())
+        })?;
+        window.add_menu_separator()?;
+        Ok::<_, systray::Error>(())
+    })?;
+
+    app.add_menu_separator()?;
+
+    app.add_menu_item("Quit", |window| {
+        window.quit();
+        Ok::<_, systray::Error>(())
+    })?;
+
+    println!("Waiting on message!");
+    app.wait_for_message()?;
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
+    match build_tray() {
+        Ok(..) => {
+
+        },
+        Err(..) => {
+
+        }
+    };
+
     println!("-- SERVER START --");
     let listener = TcpListener::bind("127.0.0.1:30000")?;
     for stream in listener.incoming() { handle_client(stream?); }
     println!("-- SERVER STOPPED --");
+
     Ok(())
 }
