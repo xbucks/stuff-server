982764991ae2d4308eee8b9996f290e5215e6781
xbucks <jmy.kot@gmail.com>
2023-10-18 21:58:04 +0900

    fix: update report window

--- src/screens/results.rs
+++ src/screens/results.rs
@@ -11,29 +11,46 @@ pub fn build_results() {
 
 #[derive(Default, NwgUi)]
 pub struct BasicApp {
-    #[nwg_control(size: (300, 200), position: (300, 300), title: "Report", flags: "WINDOW|VISIBLE")]
+    #[nwg_control(size: (800, 600), position: (200, 200), title: "Report", flags: "WINDOW|VISIBLE")]
     #[nwg_events( OnWindowClose: [BasicApp::say_goodbye] )]
     window: nwg::Window,
 
-    #[nwg_control(text: "Heisenberg", size: (280, 25), position: (10, 10))]
-    name_edit: nwg::TextInput,
+    #[nwg_control(collection: vec!["Morning", "Afternoon", "Evening"], size: (100, 25), position: (10, 10), selected_index: Some(0))]
+    ctl_when: nwg::ComboBox<&'static str>,
 
-    #[nwg_control(collection: vec!["one", "two"], size: (280, 25), position: (10, 40))]
-    apply_where: nwg::ComboBox<&'static str>,
+    #[nwg_control(collection: vec!["Bid", "Develop"], size: (80, 25), position: (120, 10), selected_index: Some(0))]
+    ctl_type: nwg::ComboBox<&'static str>,
 
-    #[nwg_control(text: "Say my name", size: (280, 60), position: (10, 70))]
+    #[nwg_control(collection: vec!["Upwork", "Freelancer", "LinkedIn", "Lancers", "Crowd", "Discord", "Telegram", "Others"], size: (100, 25), position: (210, 10), selected_index: Some(0))]
+    ctl_where: nwg::ComboBox<&'static str>,
+
+    #[nwg_control(text: "Amount", size: (60, 25), position: (320, 10))]
+    ctl_amount: nwg::TextInput,
+
+    #[nwg_control(text: "Hours", size: (60, 25), position: (390, 10))]
+    ctl_hours: nwg::TextInput,
+
+    #[nwg_control(text: "+", size: (40, 25), position: (650, 10))]
     #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    hello_button: nwg::Button
+    btn_add: nwg::Button,
+
+    #[nwg_control(text: "-", size: (40, 25), position: (700, 10))]
+    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
+    btn_delete: nwg::Button,
+
+    #[nwg_control(text: "?", size: (40, 25), position: (750, 10))]
+    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
+    btn_edit: nwg::Button
 }
 
 impl BasicApp {
 
     fn say_hello(&self) {
-        nwg::simple_message("Hello", &format!("Hello {}", self.name_edit.text()));
+        nwg::simple_message("Hello", &format!("Hello {}", self.ctl_amount.text()));
     }
     
     fn say_goodbye(&self) {
-        nwg::simple_message("Goodbye", &format!("Goodbye {}", self.name_edit.text()));
+        nwg::simple_message("Goodbye", &format!("Goodbye {}", self.ctl_amount.text()));
         nwg::stop_thread_dispatch();
     }
 
