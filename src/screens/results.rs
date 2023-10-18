d85c3386e08629c7cc1ee19292156aaae476aac2
xbucks <jmy.kot@gmail.com>
2023-10-18 22:52:04 +0900

    feat: add dynamic window

--- src/screens/results.rs
+++ src/screens/results.rs
@@ -1,64 +1,123 @@
-use nwd::NwgUi;
+use nwd::{NwgUi, NwgPartial};
 use nwg::NativeUi;
 
 pub fn build_results() {
     nwg::init().expect("Failed to init Native Windows GUI");
+    //nwg::Font::set_global_family("Segoe UI").expect("Failed to set default font");
+    let mut font = nwg::Font::default();
+    nwg::Font::builder()
+        .family("MS Shell Dlg")
+        .size(15)
+        .build(&mut font)
+        .expect("Failed to build font");
+    nwg::Font::set_global_default(Some(font));
 
-    let _app = BasicApp::build_ui(Default::default()).expect("Failed to build UI");
+    let _ui = ConfigDlg::build_ui(Default::default()).expect("Failed to build UI");
 
     nwg::dispatch_thread_events();
 }
 
 #[derive(Default, NwgUi)]
-pub struct BasicApp {
-    #[nwg_control(size: (800, 600), position: (200, 200), title: "Report", flags: "WINDOW|VISIBLE")]
-    #[nwg_events( OnWindowClose: [BasicApp::say_goodbye] )]
+pub struct ConfigDlg {
+    #[nwg_control(size: (500, 400), position: (300, 300), title: "DynLayout")]
+    #[nwg_events(OnInit: [ConfigDlg::init], OnResize: [ConfigDlg::size], OnWindowClose: [ConfigDlg::exit])]
     window: nwg::Window,
 
-    #[nwg_control(collection: vec!["Morning", "Afternoon", "Evening"], size: (100, 25), position: (10, 10), selected_index: Some(0))]
-    ctl_when: nwg::ComboBox<&'static str>,
+    #[nwg_layout(parent: window)]
+    layout: nwg::DynLayout,
 
-    #[nwg_control(collection: vec!["Bid", "Develop", "Chat"], size: (80, 25), position: (120, 10), selected_index: Some(0))]
-    #[nwg_events( OnComboxBoxSelection: [BasicApp::on_type_changed] )]
-    ctl_type: nwg::ComboBox<&'static str>,
+    #[nwg_control(position: (10, 30), size: (220, 330), collection: vec!["People"])]
+    list: nwg::ListBox<&'static str>,
 
-    #[nwg_control(collection: vec!["Upwork", "Freelancer", "LinkedIn", "Lancers", "Crowd", "Discord", "Telegram", "Others"], size: (100, 25), position: (210, 10), selected_index: Some(0))]
-    ctl_where: nwg::ComboBox<&'static str>,
+    #[nwg_control(text: "Cancel", position: (10, 350), size: (100, 25))]
+    cancel_btn: nwg::Button,
 
-    #[nwg_control(text: "Amount", size: (60, 25), position: (320, 10))]
-    ctl_amount: nwg::TextInput,
+    #[nwg_control(text: "Ok", position: (120, 350), size: (100, 25))]
+    ok_btn: nwg::Button,
 
-    #[nwg_control(text: "Hours", size: (60, 25), position: (390, 10))]
-    ctl_hours: nwg::TextInput,
+    #[nwg_control(text: "Config", position: (380, 350), size: (100, 25))]
+    config_btn: nwg::Button,
 
-    #[nwg_control(text: "+", size: (40, 25), position: (650, 10))]
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    btn_add: nwg::Button,
+    #[nwg_control(position: (240, 30), size: (240, 300))]
+    frame: nwg::Frame,
 
-    #[nwg_control(text: "-", size: (40, 25), position: (700, 10))]
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    btn_delete: nwg::Button,
-
-    #[nwg_control(text: "?", size: (40, 25), position: (750, 10))]
-    #[nwg_events( OnButtonClick: [BasicApp::say_hello] )]
-    btn_edit: nwg::Button,
-
-    #[nwg_control(text: "Description", size: (460, 160), position: (10, 50))]
-    ctl_note: nwg::TextBox,
+    #[nwg_partial(parent: frame)]
+    #[nwg_events((save_btn, OnButtonClick): [ConfigDlg::save])]
+    controls: Controls,
 }
 
-impl BasicApp {
-    fn on_type_changed(&self) {
+impl ConfigDlg {
+    fn init(&self) {
+        self.frame.set_visible(true);
 
+        self.layout.add_child((0, 0), (50, 100), &self.list);
+        self.layout.add_child((0, 100), (0, 0), &self.ok_btn);
+        self.layout.add_child((0, 100), (0, 0), &self.cancel_btn);
+        self.layout.add_child((100, 100), (0, 0), &self.config_btn);
+
+        self.layout.add_child((50, 0), (50, 100), &self.frame);
+
+        self.controls.init(&self.frame);
+
+        self.layout.fit();
     }
 
-    fn say_hello(&self) {
-        nwg::simple_message("Hello", &format!("Hello {}", self.ctl_amount.text()));
+    fn size(&self) {
+        self.layout.fit();
     }
-    
-    fn say_goodbye(&self) {
-        nwg::simple_message("Goodbye", &format!("Goodbye {}", self.ctl_amount.text()));
+
+    fn save(&self) {
+        nwg::simple_message("Saved!", "Data saved!");
+    }
+
+    fn exit(&self) {
         nwg::stop_thread_dispatch();
     }
+}
 
+#[derive(Default, NwgPartial)]
+pub struct Controls {
+    #[nwg_layout]
+    layout: nwg::DynLayout,
+
+    #[nwg_control(text: "When:", h_align: HTextAlign::Right, position: (10, 10), size: (100, 20))]
+    label1: nwg::Label,
+    #[nwg_control(text: "What:", h_align: HTextAlign::Right, position: (10, 40), size: (100, 20))]
+    label2: nwg::Label,
+
+    // #[nwg_control(text: "John Doe", position: (120, 10), size: (100, 20))]
+    // #[nwg_events(OnChar: [print_char(EVT_DATA)])]
+    // name_input: nwg::TextInput,
+    // #[nwg_control(text: "75", flags: "NUMBER|VISIBLE", position: (120, 40), size: (100, 20))]
+    // age_input: nwg::TextInput,
+    // #[nwg_control(text: "Programmer", position: (120, 70), size: (100, 25))]
+    // job_input: nwg::TextInput,
+
+    #[nwg_control(collection: vec!["Morning", "Afternoon", "Evening"], position: (120, 10), size: (100, 20), selected_index: Some(0))]
+    when_cbx: nwg::ComboBox<&'static str>,
+
+    #[nwg_control(collection: vec!["Bid", "Develop", "Chat"], position: (120, 40), size: (100, 20), selected_index: Some(0))]
+    #[nwg_events(OnComboxBoxSelection: [print_char(EVT_DATA)])]
+    type_cpx: nwg::ComboBox<&'static str>,
+
+    #[nwg_control(text: "Save", position: (10, 250), size: (100, 25))]
+    save_btn: nwg::Button,
+}
+
+impl Controls {
+    fn init(&self, frame: &nwg::Frame) {
+        self.layout.parent(frame);
+
+        self.layout.add_child((0, 0), (0, 0), &self.label1);
+        self.layout.add_child((0, 0), (0, 0), &self.label2);
+
+        self.layout.add_child((0, 0), (100, 0), &self.when_cbx);
+        self.layout.add_child((0, 0), (100, 0), &self.type_cpx);
+
+        self.layout.add_child((0, 100), (0, 0), &self.save_btn);
+    }
+}
+
+fn print_char(data: &nwg::EventData) {
+    println!("{:?}", data.on_char());
 }
